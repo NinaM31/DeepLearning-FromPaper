b587cd146d30531819b41a3dfb93e5a74b3fe0a3
NinaM31 <57009004+NinaM31@users.noreply.github.com>
2021-12-28 22:15:15 +0300

    Added config

---
+++ CRNN/config.py
@@ -0,0 +1,14 @@
+TRAIN_DIR = './CRNN/CAPTCHA/train'
+VAL_DIR = './CRNN/CAPTCHA/val'
+
+BATCH_SIZE = 8
+N_WORKERS = 0
+
+CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789'
+VOCAB_SIZE = len(CHARS)
+
+lr = 0.02
+weight_decay = 1e-5
+momentum = 0.8
+
+EPOCHS = 1
