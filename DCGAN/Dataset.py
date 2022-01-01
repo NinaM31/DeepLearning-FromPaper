e5c148835bc9691fa1abc60f49e66521435288b2
NinaM31 <57009004+NinaM31@users.noreply.github.com>
2022-01-01 17:40:41 +0300

    Added Dataset class

---
+++ DCGAN/Dataset.py
@@ -0,0 +1,30 @@
+import os
+
+import torch
+from torchvision import transforms
+from PIL import Image
+
+class Dataset(torch.utils.data.Dataset):
+
+    def __init__(self, img_dir):
+        path_list = os.listdir(img_dir)
+        abspath = os.path.abspath(img_dir)
+
+        self.img_dir = img_dir
+        self.img_list = [os.path.join(abspath, path) for path in path_list]
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor()
+        ])
+
+
+    def __len__(self):
+        return len(self.img_list)
+
+
+    def __getitem__(self, index):
+        path = self.img_list[index]
+        img = Image.open(path).convert('RGB')
+
+        img_tensor = self.transform(img)
+        return img_tensor
