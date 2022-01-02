ad208e6af17b406b05daa295f0431afaf6de61ee
NinaM31 <57009004+NinaM31@users.noreply.github.com>
2022-01-02 22:35:27 +0300

    Update Architecture

--- DCGAN/Discriminator.py
+++ DCGAN/Discriminator.py
@@ -1,46 +1,32 @@
+import torch
 import torch.nn as nn
+
 
 class Discriminator(nn.Module):
 
     def __init__(self, conv_dim=32):
         super(Discriminator, self).__init__()
 
-        self.conv_dim = conv_dim
-
-        self.conv1 = self.conv_layer(3, conv_dim, batch_norm=False)
-        self.conv2 = self.conv_layer(conv_dim, conv_dim*2)
-        self.conv3 = self.conv_layer(conv_dim*2, conv_dim*4)
-        self.conv4 = self.conv_layer(conv_dim*4, conv_dim*8)
-        self.conv5 = self.conv_layer(conv_dim*8, conv_dim*8, stride=1, padding=0)
-        
-        self.fc = nn.Sequential(
-            nn.Linear(conv_dim*8, 1),
+        self.main = nn.Sequential(
+            nn.Conv2d(3, conv_dim, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+   
+            nn.Conv2d(conv_dim, conv_dim * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim * 2),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(conv_dim * 2, conv_dim * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim * 4),
+            nn.LeakyReLU(0.2, inplace=True),
+            
+            nn.Conv2d(conv_dim * 4, conv_dim * 8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim * 8),
+            nn.LeakyReLU(0.2, inplace=True),
+          
+            nn.Conv2d(conv_dim * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
         )
-        
-
-
-    def conv_layer(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, batch_norm=True):
-        layers = []
-        conv_layer = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False)
-        
-        layers.append(conv_layer)
-
-        if batch_norm:
-            layers.append(nn.BatchNorm2d(out_channels))
-
-        layers.append(nn.LeakyReLU(negative_slope=0.2))
-        return nn.Sequential(*layers)
 
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.conv5(x)
-
-        x = x.view(-1, self.conv_dim*8)
-
-        x = self.fc(x)  
-        return x
+        return self.main(x)
--- DCGAN/Generator.py
+++ DCGAN/Generator.py
@@ -4,44 +4,31 @@ from torch import nn as nn
 
 class Generator(nn.Module):
     
-    def __init__(self, z_size, conv_dim):
+    def __init__(self, z_size, conv_dim=64):
         super(Generator, self).__init__()
 
-        self.conv_dim = conv_dim
-        self.fc = nn.Linear(z_size, conv_dim*8)
-        
-        self.tconv1 = self.deconv_layer(conv_dim*8, conv_dim*8, stride=1, padding=0)
-        self.tconv2 = self.deconv_layer(conv_dim*8, conv_dim*4)
-        self.tconv3 = self.deconv_layer(conv_dim*4, conv_dim*2)
-        self.tconv4 = self.deconv_layer(conv_dim*2, conv_dim)
-
-        self.tconv5 = self.deconv_layer(conv_dim, 3, batch_norm=False, activation=False)
-        self.dropout = nn.Dropout(0.25)
-
-    
-    def deconv_layer(self, in_channels, out_channels, kernel_size=4, stride=2, padding=1, batch_norm=True, activation=True):
-        layers = []
-
-        layers.append(nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False))
-
-        if batch_norm:
-            layers.append(nn.BatchNorm2d(out_channels))
-
-        layers.append(nn.ReLU())
-        return nn.Sequential(*layers)
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d( z_size, conv_dim * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(conv_dim * 8),
+            nn.ReLU(True),
+            
+            nn.ConvTranspose2d(conv_dim * 8, conv_dim * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim * 4),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d( conv_dim * 4, conv_dim * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim * 2),
+            nn.ReLU(True),
+           
+            nn.ConvTranspose2d( conv_dim * 2, conv_dim, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(conv_dim),
+            nn.ReLU(True),
+          
+            nn.ConvTranspose2d( conv_dim, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
 
 
     def forward(self, x):
-        x = self.fc(x)
-
-        x = x.view(-1, self.conv_dim*8, 1, 1) 
-        
-        x = self.tconv1(x)
-        x = self.tconv2(x)
-        x = self.tconv3(x)
-        x = self.tconv4(x)
-      
-        x = self.dropout(x)
-            
-        x = self.tconv5(x) 
-        return torch.tanh(x)
+        x = x.view(x.size(0), x.size(1), 1, 1) 
+        return self.main(x)
