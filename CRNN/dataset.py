2ddca91f6e8f772f7dacde4779b8d5ceac6fc46b
NinaM31 <57009004+NinaM31@users.noreply.github.com>
2022-05-13 00:58:50 +0300

    have images in size 200x100

--- CRNN/dataset.py
+++ CRNN/dataset.py
@@ -15,9 +15,8 @@ class Dataset(torch.utils.data.Dataset):
         self.img_list = [os.path.join(abspath, path) for path in path_list]
 
         self.transform = transforms.Compose([
-            # transforms.Resize((224,224)),
+            transforms.Resize((200,100)),
             transforms.ToTensor(),
-            # transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
         
 
